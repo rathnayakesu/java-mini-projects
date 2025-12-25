6deaa56e91edcbe9f13b555fdf45d5b2d2d5f048
ShanukaRathnayake <shanukaushantha2004@gmail.com>
2025-12-25 22:06:57 +0530

    Initial commit

---
+++ src/madLibsGame.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class madLibsGame {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.print("Enter an adjective (description): ");
+        System.out.print("Enter a noun (animal or person): ");
+        System.out.print("Enter an adjective (description): ");
+        System.out.print("Enter a verb end with -ing (action): ");
+        System.out.print("Enter an adjective (description): ");
+    }
+}
