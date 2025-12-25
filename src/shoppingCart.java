c0d7c4e7fd4b3e6780033bab14275d76362df9a3
ShanukaRathnayake <shanukaushantha2004@gmail.com>
2025-12-25 22:48:51 +0530

    finished

--- src/shoppingCart.java
+++ src/shoppingCart.java
@@ -1,5 +1,32 @@
-public class shoppingCart {
-    static void main(String[] args) {
+import java.util.Scanner;
 
+public class shoppingCart {
+    public static void main(String[] args) {
+
+        // SHOPPING CART PROGRAM
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        String currency = "LKR";
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + item +"/s");
+        System.out.println("Your total is " + total + " " + currency);
+
+        scanner.close();
     }
 }
