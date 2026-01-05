1d72670c01d79ec28bf8aa374875068ea34cb0c3
ShanukaRathnayake <shanukaushantha2004@gmail.com>
2026-01-06 04:44:55 +0530

    get started

---
+++ src/hangman.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class hangman {
+    public static void main(String[] args) {
+
+        String word = "pizza";
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        System.out.println("****************************");
+        System.out.println("WELCOME TO THE HANGMAN GAME!");
+        System.out.println("****************************");
+        System.out.println();
+
+        System.out.print("Word: ");
+
+
+
+
+    }
+}
